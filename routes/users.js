1bdf757f206722160a00537b68d2e2e6b0679d97
abdulhaqani <ahaqa011@uottawa.ca>
2021-02-06 01:51:04 -0500

    Authentication done. Need to integrate with DB

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,87 @@
+/* eslint-disable eqeqeq */
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+// Load user model
+
 const router = express.Router();
 
-// GET to api/users
-// Get the users
-router.post('/', (req, res) => {
-  res.send('bs');
+// login success response
+router.get('/login-success', (req, res) => {
+  res.json({ msg: 'login success' });
 });
 
-// POST to api/users
-// User Registration
-router.post('/', (req, res) => {
-  res.send('bs');
+// login failure response
+router.get('/login-failure', (req, res) => {
+  res.json({ msg: 'login failure' });
+});
+
+// login form post
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/api/users/login-success',
+    failureRedirect: '/api/users/login-failure',
+    failureFlash: true,
+  })(req, res, next);
+});
+
+// register form post
+router.post('/register', async (req, res) => {
+  const errors = [];
+
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: 'Passwords do not match' });
+  }
+  if (req.body.password.length < 6) {
+    errors.push({ text: 'Password must be at least 6 characters' });
+  }
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      password: req.body.password,
+      password2: req.body.password2,
+    });
+  } else {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      req.flash('error_msg', 'email already registered');
+      res.redirect('/users/register');
+    } else {
+      const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        password: req.body.password,
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          try {
+            let saveUser = await newUser.save() //when fail its goes to catch
+            console.log(saveUser); //when success it print.
+            console.log('after save');
+            res.json({msg: "User Saved"})
+          } catch (err) {
+            console.log('err' + err);
+            res.status(500).send(err);
+          }
+        });
+      });
+    }
+  }
+});
+
+// Logout user
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.json({msg: "logged out"})
 });
 
 module.exports = router;
